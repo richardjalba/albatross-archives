f12c5636309d271ccc5db6c98c6b079d11e8b589
Richard <richardjalba@gmail.com>
2021-01-24 14:53:47 -0800

    Fixed API endpoint, refactored Home page

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { popularGamesURL } from '../api';
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from '../api';
 
 // Action creator
 
 export const loadGames = () => async (dispatch) => {
   // FETCH AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const upcomingData = await axios.get(upcomingGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
 
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
